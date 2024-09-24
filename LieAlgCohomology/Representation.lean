4a7f5b4b987f695f68332111e3571ca097459357
xyzw12345 <wangjt2020@163.com>
2024-09-24 10:48:14 +0800

    Update Representation.lean

--- LieAlgCohomology/Representation.lean
+++ LieAlgCohomology/Representation.lean
@@ -10,53 +10,50 @@ import LieAlgCohomology.MissingLemmas.Module
 #check MonoidAlgebra
 #check LieModuleHom
 
-namespace LieDerivationCat
+namespace LieModuleCat
 
 open CategoryTheory
 
 universe u v w
 variable (R : Type u) (L : Type v) [CommRing R] [LieRing L] [LieAlgebra R L]
 
-structure LieDerivationCat where
+structure LieModuleCat where
   carrier : Type w
   [isAddCommGroup : AddCommGroup carrier]
   [isModule : Module R carrier]
   [isLieRingModule : LieRingModule L carrier]
   [isLieModule : LieModule R L carrier]
-  [isLieDerivation : LieDerivation R L carrier]
 
-instance : CoeSort (LieDerivationCat R L) (Type w) := ⟨LieDerivationCat.carrier⟩
+instance : CoeSort (LieModuleCat R L) (Type w) := ⟨LieModuleCat.carrier⟩
 
-instance (M : LieDerivationCat R L) : AddCommGroup M := M.isAddCommGroup
+instance (M : LieModuleCat R L) : AddCommGroup M := M.isAddCommGroup
 
-instance (M : LieDerivationCat R L) : Module R M := M.isModule
+instance (M : LieModuleCat R L) : Module R M := M.isModule
 
-instance (M : LieDerivationCat R L) : LieRingModule L M := M.isLieRingModule
+instance (M : LieModuleCat R L) : LieRingModule L M := M.isLieRingModule
 
-instance (M : LieDerivationCat R L) : LieModule R L M := M.isLieModule
+instance (M : LieModuleCat R L) : LieModule R L M := M.isLieModule
 
-instance (M : LieDerivationCat R L) : LieDerivation R L M := M.isLieDerivation
-
-instance : Category (LieDerivationCat R L) where
+instance : Category (LieModuleCat R L) where
   Hom M N := LieModuleHom R L M N
   id M := LieModuleHom.id (R := R) (L := L)
   comp f g := LieModuleHom.comp g f
 
-instance {M N : LieDerivationCat R L} : FunLike (M ⟶ N) M N :=
+instance {M N : LieModuleCat R L} : FunLike (M ⟶ N) M N :=
   LieModuleHom.instFunLike (R := R) (L := L) (M := M) (N := N)
 
 @[ext]
-lemma ext {M N : LieDerivationCat R L} (f g : M ⟶ N) (h : ∀ (x : M), f x = g x) :
+lemma ext {M N : LieModuleCat R L} (f g : M ⟶ N) (h : ∀ (x : M), f x = g x) :
   f = g := DFunLike.ext _ _ h
 
-def toModuleUniversalAlgebra_objMap (M : LieDerivationCat R L) : Module (UniversalEnvelopingAlgebra R L) M :=
+def toModuleUniversalAlgebra_objMap (M : LieModuleCat R L) : Module (UniversalEnvelopingAlgebra R L) M :=
   @Module.ofAddMonoidEnd _ _ _ _ ((Module.toAddMonoidEnd (Module.End R M) M).comp
    ((UniversalEnvelopingAlgebra.lift R (L := L) (A := Module.End R (M : Type v))
     (LieModule.toEnd R L M)).1))
 
 attribute [instance] toModuleUniversalAlgebra_objMap
 
-def toModuleUniversalAlgebra : LieDerivationCat R L ⥤ ModuleCat (UniversalEnvelopingAlgebra R L) where
+def toModuleUniversalAlgebra : LieModuleCat R L ⥤ ModuleCat (UniversalEnvelopingAlgebra R L) where
   obj M := ⟨M⟩
   map {M N} f := {
     toFun := f
@@ -87,14 +84,8 @@ def ofModuleUniversalAlgebra_objMap_isLieModule (M : ModuleCat (UniversalEnvelop
 
 attribute [instance] ofModuleUniversalAlgebra_objMap_isLieModule
 
-def ofModuleUniversalAlgebra_objMap (M : ModuleCat (UniversalEnvelopingAlgebra R L)) :
-  LieDerivation R L M := by sorry
-
-def ofModuleUniversalAlgebra : ModuleCat (UniversalEnvelopingAlgebra R L) ⥤ LieDerivationCat R L where
-  obj M := {
-    carrier := M
-    isLieDerivation := ofModuleUniversalAlgebra_objMap R L M
-  }
+def ofModuleUniversalAlgebra : ModuleCat (UniversalEnvelopingAlgebra R L) ⥤ LieModuleCat R L where
+  obj M := ⟨M⟩
   map {M N} f := {
     toFun := f
     map_add' := f.map_add
@@ -109,10 +100,10 @@ attribute [-instance] ofModuleUniversalAlgebra_objMap_isLieRingModule
 attribute [-instance] ofModuleUniversalAlgebra_objMap_isModule
 
 def equivalenceLieDerivationModuleUniversalAlgebra :
-  LieDerivationCat R L ≌ ModuleCat (UniversalEnvelopingAlgebra R L) where
+  LieModuleCat R L ≌ ModuleCat (UniversalEnvelopingAlgebra R L) where
     functor := toModuleUniversalAlgebra R L
     inverse := ofModuleUniversalAlgebra R L
     unitIso := sorry
     counitIso := sorry
 
-end LieDerivationCat
+end LieModuleCat
