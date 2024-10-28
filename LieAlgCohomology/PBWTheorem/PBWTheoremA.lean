da5caaf4882f77bff0a3ae6892a4c2c664613fa5
xyzw12345 <wangjt2020@163.com>
2024-10-28 18:13:00 +0800

    Update PBWTheoremA.lean

--- LieAlgCohomology/PBWTheorem/PBWTheoremA.lean
+++ LieAlgCohomology/PBWTheorem/PBWTheoremA.lean
@@ -6,16 +6,18 @@ open BigOperators TensorProduct DirectSum TensorAlgebra UniversalEnvelopingAlgeb
 -- set_option diagnostics true
 noncomputable section
 variable (R : Type*) [CommRing R]
-variable (L : Type*) [AddCommMonoid L] [Module R L] [LieRing L] [LieAlgebra R L] [Module.Free R L]
+variable (L : Type*) [i11: AddCommMonoid L] [i12: Module R L] [i13: LieRing L] [i14: LieAlgebra R L] [Module.Free R L]
 
 local notation "ιₜ" => TensorAlgebra.ι R
 local notation "𝔘" => UniversalEnvelopingAlgebra
 local notation "π₁" => mkAlgHom
+-- local notation "𝔗" => TensorAlgebra
+
+abbrev 𝔗 := @TensorAlgebra R CommRing.toCommSemiring L i11 i12
 
 #check MvPolynomial
 #synth Algebra R (MvPolynomial L R)
 #synth Module R (TensorAlgebra R L)
-abbrev 𝔗 := TensorAlgebra R L
 
 def I := TwoSidedIdeal.span {(ιₜ x * ιₜ y - ιₜ y * ιₜ x) | (x : L) (y : L)}
 
@@ -26,28 +28,30 @@ instance : Ring (𝔖 R L) := inferInstanceAs (Ring (RingQuot (I R L).ringCon.r)
 
 instance : Algebra R (𝔖 R L) := inferInstanceAs (Algebra R (RingQuot (I R L).ringCon.r))
 
-def J := TwoSidedIdeal.span {ιₜ x * ιₜ y - ιₜ y * ιₜ x - ιₜ ⁅x, y⁆ | (x : L) (y : L)}
+-- def J := TwoSidedIdeal.span {ιₜ x * ιₜ y - ιₜ y * ιₜ x - ιₜ ⁅x, y⁆ | (x : L) (y : L)}
 
 #synth GradedRing ((LinearMap.range (ι R : L →ₗ[R] TensorAlgebra R L) ^ ·))
 
 abbrev graded_T (n : ℕ) := (LinearMap.range (ι R : L →ₗ[R] TensorAlgebra R L) ^ n)
-
-abbrev filter_T (n : ℕ) := ⨆ (m : Fin (n + 1)), (graded_T R L m.1)
-
-#check π₁ R L
-#synth FunLike (TensorAlgebra R L →ₐ[R] 𝔘 R L) (TensorAlgebra R L) (𝔘 R L)
-#check filter_T R L 2
-set_option diagnostics true
-def filter_U (n : ℕ) : Submodule R (𝔘 R L) :=
-  haveI : FunLike (TensorAlgebra R L →ₐ[R] 𝔘 R L) (TensorAlgebra R L) (𝔘 R L) := inferInstance
-  Submodule.map (σ₁₂ := RingHom.id R) (π₁ R L) (filter_T R L n)
-
 #synth GradedRing (graded_T R L)
 
-def filtration (m : ℕ) (T : Type*) [AddCommMonoid T] [Module R T] : Submodule R T := sorry
+def filter_T (n : ℕ) := ⨆ (m : Fin (n + 1)), (graded_T R L m.1)
 
-def T (m : ℕ) := filtration (R := R) m (TensorAlgebra R L)
+def filter_U (n : ℕ) : Submodule R (𝔘 R L) := sorry
+  -- it's supposed to be the following definition, but it failed due to some weird instance inference problem.
+  -- Submodule.map (π₁ R L) (filter_T R L n)
 
-def equiv_finite_directSum : T R L m ≃ₗ[R] (⨁ (i : Fin (m + 1)), ⨂[R]^i L) := sorry
+def filter_U' (n : ℕ) : Submodule R (filter_U R L n) := by sorry
 
--- theorem PBW : Nonempty (𝔖 R L →ₐ[k] 𝔊 R L) := sorry
+abbrev graded_G (n : ℕ) := (filter_U R L (n + 1)) ⧸ (filter_U' R L n)
+
+abbrev 𝔊 := ⨁ (n : ℕ), (graded_G R L n)
+
+instance : Ring (𝔊 R L) := sorry
+
+instance : Algebra R (𝔊 R L) := sorry
+
+def ω' : (𝔗 R L) →ₐ[R] (𝔊 R L) := sorry
+def ω : (𝔖 R L) →ₐ[R] (𝔊 R L) := sorry
+
+theorem PBW_A : Function.Bijective (ω R L) := sorry
